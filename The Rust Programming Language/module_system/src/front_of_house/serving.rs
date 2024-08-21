d69db0fd4e2c668a033c8d348e3020325d43f1b7
Juche <azhucheng1@qq.com>
2024-08-21 09:26:55 +0800

    add: Rust Module System Demo

---
+++ The Rust Programming Language/module_system/src/front_of_house/serving.rs
@@ -0,0 +1,3 @@
+pub fn take_order() {}
+pub fn serve_order() {}
+fn take_payment() {}
--- The Rust Programming Language/module_system/src/lib.rs
+++ The Rust Programming Language/module_system/src/lib.rs
@@ -1,15 +1,4 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        fn seat_to_table() {}
-    }
-
-    pub mod serving {
-        pub fn take_order() {}
-        pub fn serve_order() {}
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
 
 pub fn eat_at_restaurant() {
     crate::front_of_house::hosting::add_to_waitlist(); // 绝对路径调用
