95ba0d51450154ffbfc531c311dfe5f64a87612a
Juche <azhucheng1@qq.com>
2024-08-23 09:41:54 +0800

    update:: Rust 常用数据集

--- The Rust Programming Language/variables/src/main.rs
+++ The Rust Programming Language/variables/src/main.rs
@@ -63,7 +63,113 @@ fn main() {
     println!("m2 = {}", m2);
 
     // Vector
-    let mut v = vec![1, 2, 3, 4, 5];
-    v.push(6);
+    // let mut v: Vec<i32> = Vec::new(); // 通过 new 方法创建
+    let mut v = vec![1, 2, 3, 4, 5]; // 通过 vec! 宏创建
+    v.push(6); // 添加元素
     println!("v = {:?}", v);
+
+    let v_6 = v.get(6); // 通过 get 访问元素 => 超出范围不会引发程序崩溃
+    match v_6 {
+        Some(val) => println!("v_6 = {}", val),
+        None => println!("There is no v_6 val!"),
+    }
+    // let v_6 = &v[6]; // 通过索引访问元素 => 超出范围会 panic 引发程序崩溃
+    // println!("v_6 = {}", v_6);
+
+    // String
+    let mut s1 = String::new();
+    let s2 = "abc".to_string();
+    let s3 = String::from("abc");
+
+    // push_str 方法将字符串切片附加到 String
+    s1.push_str(&s2);
+    s1.push_str("def");
+
+    // push 方法把单个字符附加到 String
+    s1.push('g');
+
+    println!("s1 = {}", s1);
+
+    // + 连接字符串 `String + &str`, 会获取第一个参数的所有权
+    let s4 = s2 + &s3;
+    println!("s4 = {}", s4);
+
+    // format! 宏拼接字符串,更灵活,不会获取任何参数的所有权
+    let s5 = String::from("ABC");
+    let s6 = String::from("DEF");
+    let s7 = String::from("GHI");
+    // let s8 = s5 + "-" + &s6 + "-" + &s7;
+    let s8 = format!("{}-{}-{}", s5, s6, s7);
+    println!("s8 = {}", s8);
+
+    // len() 方法获取字节长度
+    let s1_len = s1.len();
+
+    // String 不支持索引形式获取字符 &s1[0]
+    // 因为不同编码规范的字符对应的 Unicode 标量值不一致
+    // 字节(Bytes) => bytes() 方法
+    // 标量值(Scalar Values) => chars() 方法
+    // 字形簇(Grapheme Cluster) => 最接近 `字母` 的概念, 标准库未提供对应的方法,可寻找对应的第三方库处理
+
+    // 切割 String, 可通过 [] 和一个范围来创建字符串的切片(包含起始索引,不包含终止索引)
+    // 需谨慎使用,沿字符边界切割(某些语言一个字符包含多个字节)
+    let s9 = &s1[0..2];
+
+    // HashMap
+    use std::collections::HashMap; // 不在 prelude 模块中,需手动导入
+
+    // let mut scores: HashMap<String, i32> = HashMap::new();
+    let mut scores = HashMap::new(); // 创建
+    scores.insert(String::from("Blue"), 10); // 添加数据
+    scores.insert(String::from("Yellow"), 50); // 添加数据
+
+    // collect 方法可以将数据整合成多种数据类型, 包括 HashMap (返回值需显式指明类型)
+    // 将 teams 和 init_score 遍历项缝合组成元组数组, 然后通过 collect 创建指定的 HashMap 类型
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let init_score = vec![10, 50];
+    let scores: HashMap<_, _> = teams.iter().zip(init_score.iter()).collect();
+
+    // get 方法访问 HashMap 中的值, 参数: K, 返回 Option<&V>
+    // let blue_score = scores.get(&String::from("blue"));
+    let blue_score = scores.get(&String::from("Blue"));
+    match blue_score {
+        Some(val) => println!("blue_score = {}", val),
+        None => println!("blue_score not found!"),
+    }
+
+    // for 循环遍历 HashMap
+    for (k, v) in &scores {
+        println!("{}: {}", k, v);
+    }
+
+    // 更新 HashMap
+    // 新增值/使用新值替换现有值 `HashMap.insert(K, V)`
+    let mut person = HashMap::new();
+    person.insert(String::from("name"), String::from("Juching"));
+    person.insert(String::from("name"), String::from("Juche"));
+    println!("person = {:#?}", person);
+
+    // 保留现有值,忽略新值 `HashMap.entry(K).or_insert(V)`
+    let new_age = person
+        .entry(String::from("age"))
+        .or_insert("18".to_string());
+    println!("new_age = {}", new_age);
+    let new_name = person
+        .entry(String::from("name"))
+        .or_insert(String::from("JU"));
+    println!("new_name = {}", new_name);
+    println!("person = {:#?}", person);
+
+    // 合并新值和现有值
+    let text = "hello world wonderful world !";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+        println!("count = {}", count);
+    }
+
+    println!("map = {:#?}", map);
 }
