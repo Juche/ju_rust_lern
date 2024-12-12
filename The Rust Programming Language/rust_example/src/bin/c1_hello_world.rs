9d53b8b13d04a71740d8b6576e4a44fb9995c984
Juche <azhucheng1@qq.com>
2024-12-12 17:28:41 +0800

    rust_example project add c1_hello_world crate

---
+++ The Rust Programming Language/rust_example/src/bin/c1_hello_world.rs
@@ -0,0 +1,6 @@
+use rust_example::c1_hello_world::comment::Person;
+
+fn main() {
+    let person = Person::new("Juching");
+    person.hello();
+}
---
+++ The Rust Programming Language/rust_example/src/lib.rs
@@ -0,0 +1,3 @@
+#![crate_name = "rust_example"]
+
+pub mod c1_hello_world;
