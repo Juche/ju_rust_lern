3f716d8e82d8b8093d78fb09dd59acb2d421c9c8
Juche <azhucheng1@qq.com>
2024-12-13 09:59:18 +0800

    update: rust_example project add c1_hello_world crate comment module

--- The Rust Programming Language/rust_example/src/c1_hello_world/mod.rs
+++ The Rust Programming Language/rust_example/src/c1_hello_world/mod.rs
@@ -1,1 +1,5 @@
+//! # c1_hello_world
+//!
+//! `c1_hello_world` 包含 `comment` 模块和 `format` 模块。
+
 pub mod comment;
--- The Rust Programming Language/rust_example/src/lib.rs
+++ The Rust Programming Language/rust_example/src/lib.rs
@@ -1,3 +1,11 @@
+//! # rust_example
+//!
+//! `rust_example` 项目是基于文档 [通过例子学 Rust 中文版](https://rustwiki.org/zh-CN/rust-by-example/index.html) 的学习示例集合
+//!
+//! 根据文档章节为 `lib crate` 创建对应的 `module`
+//!
+//! 并在 `bin` 目录下创建对应的 `bin crate` 中执行对应的 `module`
+
 #![crate_name = "rust_example"]
 
 pub mod c1_hello_world;
