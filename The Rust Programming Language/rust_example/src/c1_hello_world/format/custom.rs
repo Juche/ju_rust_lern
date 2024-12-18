3df4fc2bb6cda1b6e9bdc35f6edcea982ea2c87b
Juche <azhucheng1@qq.com>
2024-12-18 12:06:51 +0800

    update format module

--- The Rust Programming Language/rust_example/src/c1_hello_world/format/custom.rs
+++ The Rust Programming Language/rust_example/src/c1_hello_world/format/custom.rs
@@ -35,9 +35,112 @@ impl fmt::Display for Vec2D {
     }
 }
 
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    image: f64,
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} + {}i", self.real, self.image)
+    }
+}
+
+struct List(Vec<i32>);
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec = &self.0;
+        write!(f, "[")?;
+        for (i, v) in vec.iter().enumerate() {
+            if i != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{i}: {v}")?;
+        }
+        write!(f, "]")
+    }
+}
+
+struct City {
+    name: &'static str,
+    lat: f32,
+    lon: f32,
+}
+
+impl fmt::Display for City {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let lat_ori = if self.lat > 0.0 { "N" } else { "S" };
+        let lon_ori = if self.lon > 0.0 { "E" } else { "W" };
+
+        write!(
+            f,
+            "{}: {:.3}°{}, {:.3}°{}",
+            self.name,
+            self.lat.abs(),
+            lat_ori,
+            self.lon.abs(),
+            lon_ori
+        )
+    }
+}
+
+#[derive(Debug)]
+struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "RGB ({r}, {g}, {b})", r = self.r, g = self.g, b = self.b)
+    }
+}
+
 pub fn run() {
     let v = Vec2D { x: 3, y: 4 };
-
     println!("{v}"); // println!("{}", v);
     println!("{v:?}"); // println!("{:?}", v);
+
+    let c = Complex {
+        real: 3.3,
+        image: 7.2,
+    };
+    println!("{c}");
+    println!("{c:?}");
+
+    let l = List(vec![1, 2, 3]);
+    println!("{l}");
+
+    for city in [
+        City {
+            name: "Dublin",
+            lat: 53.347778,
+            lon: -6.259722,
+        },
+        City {
+            name: "Oslo",
+            lat: 59.95,
+            lon: 10.75,
+        },
+    ]
+    .iter()
+    {
+        println!("{}", *city);
+    }
+
+    for color in [
+        Color {
+            r: 128,
+            g: 255,
+            b: 90,
+        },
+        Color { r: 0, g: 3, b: 254 },
+    ]
+    .iter()
+    {
+        println!("{}", *color);
+        println!("{:?}", *color);
+    }
 }
